a7b62755079ddf0c5ca96fdb9cf2733a26fa34cc
João Victor de Oliveira Moreira <20231084010010@ifrn.local>
2026-06-29 08:21:19 -0300

    estrutura de login

--- routes.py
+++ routes.py
@@ -3,3 +3,7 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/cad_login')
+def login():
+    return render_template('cad_login.html')
