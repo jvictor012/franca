bfa613b5c0d2bff365b1da26fe2ed54f8a524783
João Victor de Oliveira Moreira <20231084010010@ifrn.local>
2026-06-29 08:06:37 -0300

    estrutura ok

---
+++ main.py
@@ -0,0 +1,4 @@
+from index import app
+from routes import *
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ routes.py
@@ -0,0 +1,5 @@
+from index import app
+from flask import render_template
+@app.route('/')
+def home():
+    return render_template('home.html')
